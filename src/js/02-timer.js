1e6ec0db5adec1d4b9fd9835615cf7540eabe924
Arthur Kravchenko <arturkra92@gmail.com>
2022-02-02 01:07:23 +0200

    add timer

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,19 +1,74 @@
-// import flatpickr from "flatpickr";
-// import 'flatpickr/dist/flatpickr.min.css';
-// import Notiflix from "notiflix";
-// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import flatpickr from "flatpickr";
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from "notiflix";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-// const options = {
-// enableTime: true,
-// time_24hr: true,
-// defaultDate: new Date(),
-// minuteIncrement: 1,
-// onClose(selectedDates) {
-//     console.log(selectedDates[0]);
-//     if (selectedDates[0] < new Date()) {
-//         Notiflix.Notify.failure("Please choose a date in the future")
-//     }
-//     else {
-//         Notiflix.Notify.success('Чтобы запустить таймер нажмите кнопку Start');
-//     }
-// }
+const refs = {
+    startBtn: document.querySelector('[data-start]'),
+    timerInput: document.querySelector('#datetime-picker'),
+    tDay: document.querySelector('[data-days]'),
+    tHours: document.querySelector('[data-hours]'),
+    tMinutes: document.querySelector('[data-minutes]'),
+    tSeconds: document.querySelector('[data-seconds]'),
+}
+
+refs.startBtn.disabled = true; 
+
+
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+        console.log(selectedDates[0]);
+        if (selectedDates[0] < new Date()) {
+            refs.timerInput.style.borderColor = 'red';
+            Notiflix.Notify.failure('Please choose a date in the future');
+        } else {
+            Notiflix.Notify.success('Please push the "Start" button')
+            refs.startBtn.disabled = false; 
+            refs.timerInput.style.borderColor = 'green';
+
+            refs.startBtn.addEventListener('click', () => {
+                let timeId = setInterval(() => {
+                    const saveDate = convertMs(selectedDates[0] - new Date());
+                    refs.tDay = saveDate.days;
+                    refs.tHours = saveDate.hours;
+                    refs.tMinutes = saveDate.minutes;
+                    refs.tSeconds = saveDate.seconds;
+                    if ((selectedDates[0] - new Date()) < 1000) {
+                        clearInterval(timeId);
+                    }
+                }, 1000);
+            })
+        }
+    },
+};
+
+
+function addLeadingZero(value) {
+    return String(value).padStart(2, '0');
+}
+flatpickr(refs.timerInput, options);
+
+function convertMs(ms) {
+
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
+
+const days = addLeadingZero(Math.floor(ms / day));
+const hours = addLeadingZero(Math.floor((ms % day) / hour));
+const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+
+return { days, hours, minutes, seconds };
+}
+
+
+
+
+
+
