3ef6638fa6f9c0c5845585e0f7ddd3f361848ed7
Arthur Kravchenko <arturkra92@gmail.com>
2022-02-01 20:23:57 +0200

    add color switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let colorTimeId = null; 
+
+startBtn.addEventListener('click', colorSwitcher);
+stopBtn.addEventListener('click', colorSwitchStopper)
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function colorSwitcher() {
+  colorTimeId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor()
+  }, 1000);
+  if (colorTimeId) {
+    startBtn.disabled = true; 
+  }
+}
+
+function colorSwitchStopper() {
+  clearInterval(colorTimeId);
+  startBtn.disabled = false; 
+}
+
+
 
---
+++ src/js/02-timer.js
@@ -0,0 +1,19 @@
+// import flatpickr from "flatpickr";
+// import 'flatpickr/dist/flatpickr.min.css';
+// import Notiflix from "notiflix";
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+// const options = {
+// enableTime: true,
+// time_24hr: true,
+// defaultDate: new Date(),
+// minuteIncrement: 1,
+// onClose(selectedDates) {
+//     console.log(selectedDates[0]);
+//     if (selectedDates[0] < new Date()) {
+//         Notiflix.Notify.failure("Please choose a date in the future")
+//     }
+//     else {
+//         Notiflix.Notify.success('Чтобы запустить таймер нажмите кнопку Start');
+//     }
+// }
