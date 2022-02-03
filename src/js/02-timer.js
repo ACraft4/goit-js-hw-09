38b395d8f4ec2b9a6637abc2abb309f10f046c34
Arthur Kravchenko <arturkra92@gmail.com>
2022-02-03 02:51:18 +0200

    fix timer and add promises tasks

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -54,17 +54,17 @@ flatpickr(refs.timerInput, options);
 
 function convertMs(ms) {
 
-const second = 1000;
-const minute = second * 60;
-const hour = minute * 60;
-const day = hour * 24;
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
 
-const days = addLeadingZero(Math.floor(ms / day));
-const hours = addLeadingZero(Math.floor((ms % day) / hour));
-const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
-const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+    const days = addLeadingZero(Math.floor(ms / day));
+    const hours = addLeadingZero(Math.floor((ms % day) / hour));
+    const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+    const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
 
-return { days, hours, minutes, seconds };
+    return { days, hours, minutes, seconds };
 }
 
 
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,48 @@
+import { Notify } from 'notiflix';
+
+const promiseForm = document.querySelector('.form'); 
+promiseForm.addEventListener('submit', onsubmit);
+
+function onsubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { delay, step, amount }
+  } = event.currentTarget;
+  
+  let delays = Number(delay.value);
+  let steps = Number(step.value);
+  let amount = Number(amount.value);
+  let position = 1;
+
+  if (delays < 0 || steps < 0) {
+    Notify.failure(`FirstDelay and Delay step - cannot be negative`);
+    return
+  }
+  for (position; position <= amount; position += 1) {
+    createPromise(position, delays)
+      .then(({ position, delays }) => {
+        setTimeout(() => {
+          Notify.success(`Fulfilled promise ${position} in ${delays}ms`)
+        }, delays);
+      }
+  })
+.catch (({ position, delays }) => {
+    setTimeout(() => {
+      Notify.failure(`Rejected promise ${position} in ${delays}ms`);
+    }, delays);
+  });
+  delays += steps;
+
+}
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    if (shouldResolve) {
+      resolve({ position, delays });
+    } else {
+      reject({ position, delays }); 
+    }
+  });
 }
